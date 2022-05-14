3a6e1c4f27a00dd5e9ede198b0991b3d52ca4afd
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-05-14 14:48:44 +0530

    add rainbowkit, dark mode, cool mode and all tutorial stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,46 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import "@rainbow-me/rainbowkit/styles.css";
+
+import {
+  apiProvider,
+  configureChains,
+  getDefaultWallets,
+  midnightTheme,
+  RainbowKitProvider,
+} from "@rainbow-me/rainbowkit";
+import { chain, createClient, WagmiProvider } from "wagmi";
+
+const { chains, provider } = configureChains(
+  [
+    chain.mainnet,
+    chain.polygon,
+    chain.goerli,
+    chain.rinkeby,
+    chain.polygonMumbai,
+  ],
+  [apiProvider.fallback()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiProvider client={wagmiClient}>
+      <RainbowKitProvider chains={chains} theme={midnightTheme()} coolMode>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
